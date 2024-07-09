e5a2c1f9e2360c39f6f945f18c5fe12dda96be56
asliddinberdiev <asliddinwork@gmail.com>
2024-07-09 15:45:49 +0500

    edit index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
-app.use(express.json());
 require('./bot/bot')
 
 async function dev() {
@@ -17,12 +13,9 @@ async function dev() {
       .then(() => console.log("mongo_db start"))
       .catch((error) => console.log(error));
 
-    // app
-    app.listen(PORT, () => console.log("server running"));
-
   } catch (error) {
     console.log(error);
   }
 }
 
-dev().then()
+dev()
